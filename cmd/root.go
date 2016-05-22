ee65d977568562fe5dfab1969b8e32335b4919e7
Eddy Zagabe <zagabe.ed@gmail.com>
2016-05-22 16:41:22 +0200

    root-cmd: Kafkactl description added

--- cmd/root.go
+++ cmd/root.go
@@ -27,16 +27,9 @@ var cfgFile string
 // This represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "kafkactl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-// Uncomment the following line if your bare application
-// has an action associated with it:
-//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "A simple REST client for the scheduler remote API",
+	Long: `Kafkactl is a command line tool written in GO for controlling kafka scheduler
+that runs on top of Apache Mesos.`,
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -68,8 +61,8 @@ func initConfig() {
 	}
 
 	viper.SetConfigName(".kafkactl") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")  // adding home directory as first search path
-	viper.AutomaticEnv()          // read in environment variables that match
+	viper.AddConfigPath("$HOME")     // adding home directory as first search path
+	viper.AutomaticEnv()             // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {

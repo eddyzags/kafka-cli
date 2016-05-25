9d7778779038756deb968d4039b0ec852af119ac
Eddy Zagabe <zagabe.ed@gmail.com>
2016-05-25 12:03:55 +0200

    api-client: Minor error fix (imports)

--- api/client/client.go
+++ api/client/client.go
@@ -1,10 +1,18 @@
 package client
 
+import (
+	"strings"
+)
+
 type Client struct {
 	URL string
 }
 
 func NewClient(apiURL string) *Client {
+	if !strings.HasPrefix(apiURL, "http://") {
+		apiURL = "http://" + apiURL
+	}
+
 	return &Client{
 		URL: apiURL,
 	}

c0cfd3de3d4b636292e1f7417200f32041f3301c
Eddy Zagabe <zagabe.ed@gmail.com>
2016-05-25 12:04:45 +0200

    cmd: RootCmd config file removed

--- cmd/root.go
+++ cmd/root.go
@@ -45,11 +45,7 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports Persistent Flags, which, if defined here,
-	// will be global for your application.
-
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kafkactl.yaml)")
+	//	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kafkactl.yaml)")
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")

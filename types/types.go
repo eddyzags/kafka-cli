25c69cd84e1c94a8ef28fb5b1377b4db02ef9618
Eddy Zagabe <zagabe.ed@gmail.com>
2016-05-27 16:31:20 +0200

    types: BrokerAdd parameters added

--- types/types.go
+++ types/types.go
@@ -1,10 +1,19 @@
 package types
 
 type BrokerAdd struct {
-	Expr     string
-	Cpus     float64
-	Mem      float64
-	Heap     float64
-	Delay    string
-	MaxDelay string
+	BindAddress      string
+	Constraints      string
+	Cpus             float64
+	Expr             string
+	FailoverDelay    string
+	FailoverMaxDelay string
+	FailoverMaxTries string
+	Heap             float64
+	JvmOptions       string
+	Log4jOptions     string
+	Mem              float64
+	Options          string
+	Port             string
+	StickinessPeriod string
+	Volume           string
 }

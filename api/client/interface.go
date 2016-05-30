da1b2851f24bf34447856ce3b6b2b77519804904
Eddy Zagabe <zagabe.ed@gmail.com>
2016-05-30 15:44:03 +0200

    api-client: BrokerStop client function added to client interface

--- api/client/interface.go
+++ api/client/interface.go
@@ -9,4 +9,5 @@ type APIClient interface {
 	BrokerAdd(in map[string]string) ([]*models.Broker, error)
 	BrokerStart(expr, timeout string) ([]*models.Broker, error)
 	BrokerList() ([]*models.Broker, error)
+	BrokerStop(expr string) ([]*models.Broker, error)
 }

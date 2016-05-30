cfef0634baa2cce29c1dfd496ebae068cfbd3ecd
Eddy Zagabe <zagabe.ed@gmail.com>
2016-05-31 00:46:33 +0200

    api-client: BrokerUpdate added to client interface

--- api/client/interface.go
+++ api/client/interface.go
@@ -8,6 +8,7 @@ type APIClient interface {
 	// Broker handling functions
 	BrokerAdd(in map[string]string) ([]*models.Broker, error)
 	BrokerStart(expr, timeout string) ([]*models.Broker, error)
+	BrokerUpdate(in map[string]string) ([]*models.Broker, error)
 	BrokerList() ([]*models.Broker, error)
 	BrokerStop(expr, timeout string, stop bool) ([]*models.Broker, error)
 	BrokerRemove(expr string) (string, error)

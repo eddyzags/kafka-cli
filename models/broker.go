6e5c2fe6e57e72264b1bbaf43517679c04aab872
Eddy Zagabe <zagabe.ed@gmail.com>
2016-05-31 13:08:53 +0200

    models: BrokerStickiness struct added

--- models/broker.go
+++ models/broker.go
@@ -17,13 +17,19 @@ type BrokerTask struct {
 	State      string `json:"state"`
 }
 
+type BrokerStickiness struct {
+	Hostname string `json:"hostname"`
+	Period   string `json:"period"`
+}
+
 type Broker struct {
-	ID       string          `json:id`
-	Mem      float64         `json:"mem"`
-	Cpus     float64         `json:"cpus"`
-	Heap     float64         `json:"heap"`
-	Failover *BrokerFailover `json:"failover"`
-	Active   bool            `json:"active"`
-	Task     *BrokerTask     `json:"task"`
-	Port     string          `json:"port"`
+	ID         string            `json:id`
+	Mem        float64           `json:"mem"`
+	Cpus       float64           `json:"cpus"`
+	Heap       float64           `json:"heap"`
+	Failover   *BrokerFailover   `json:"failover"`
+	Stickiness *BrokerStickiness `json:"stickiness"`
+	Active     bool              `json:"active"`
+	Task       *BrokerTask       `json:"task"`
+	Port       string            `json:"port"`
 }
